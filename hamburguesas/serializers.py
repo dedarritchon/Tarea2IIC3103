a2fb7d10f4200c95377b3246b279189187ae3b8f
Daniel Darritchon <dedarritchon@uc.cl>
2020-04-18 20:22:11 -0400

    added ingredients path on hamburguer dict

--- hamburguesas/serializers.py
+++ hamburguesas/serializers.py
@@ -2,13 +2,26 @@ from rest_framework import serializers
 from .models import Hamburguesa, Ingrediente
 
 
-class HamburguesaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hamburguesa
-        fields = ('id', 'nombre', 'precio', 'descripcion', 'imagen', 'ingredientes')
-
 class IngredienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingrediente
         fields = ('id', 'nombre', 'descripcion')
+
+class CustomIngredienteSerializer(IngredienteSerializer):
+
+    def to_representation(self, instance):
+        data = super(IngredienteSerializer, self).to_representation(instance)
+        base_url = "https://tarea2iic3103.herokuapp.com/ingrediente/"
+        path = base_url + str(data['id'])
+        return {'path': path}
+
+
+
+class HamburguesaSerializer(serializers.ModelSerializer):
+    ingredientes = CustomIngredienteSerializer(many=True)
+    class Meta:
+        model = Hamburguesa
+        fields = ('id', 'nombre', 'precio', 'descripcion', 'imagen', 'ingredientes')
+
+
 
