5901c6861abc5f0759a568ce00a9520333c65e66
Daniel Darritchon <dedarritchon@uc.cl>
2020-04-27 21:06:20 -0400

    precio field is required

--- hamburguesas/models.py
+++ hamburguesas/models.py
@@ -13,7 +13,7 @@ class Ingrediente(models.Model):
 class Hamburguesa(models.Model):
 
     nombre = models.CharField(max_length=100)
-    precio = models.IntegerField(default=0)
+    precio = models.IntegerField()
     descripcion = models.CharField(max_length=255)
     imagen = models.URLField(max_length=255)
     ingredientes = models.ManyToManyField(Ingrediente, blank=True)
