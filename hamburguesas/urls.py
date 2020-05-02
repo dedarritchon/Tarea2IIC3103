530ecd4093e3e1b84355963690ae1bf58a325abf
Daniel Darritchon <dedarritchon@uc.cl>
2020-05-02 12:47:44 -0400

    quito slash de todas las rutas

--- hamburguesas/urls.py
+++ hamburguesas/urls.py
@@ -5,9 +5,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('hamburguesa/', views.hamburguesa_list, name='hamburguesas'),
+    path('hamburguesa', views.hamburguesa_list, name='hamburguesas'),
     path('hamburguesa/<pk>', views.hamburguesa_detail, name='hamburguesa'),
-    path('ingrediente/', views.ingrediente_list, name='ingredientes'),
+    path('ingrediente', views.ingrediente_list, name='ingredientes'),
     path('ingrediente/<pk>', views.ingrediente_detail, name='ingrediente'),
     path('hamburguesa/<h_pk>/ingrediente/<i_pk>', views.hamburguesa_edit, name='edit_hamburguer'),
 ]
